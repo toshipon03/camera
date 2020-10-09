5a6c06a2d32b032b043ca7ab188fcf520cf293a6
toshipon03 <toshipon03@gmail.com>
2020-10-09 21:36:36 +0900

    camera_chenge5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get "/" => 'home#top'
   get "/home/next1" => "home#next1"
+  get "/home/pict1" => "home#pict1"
+  get "/home/pict2" => "home#pict2"
+  get "/home/pict3" => "home#pict3"
+  get "/home/pict4" => "home#pict4"
+  get "/home/pict5" => "home#pict5"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
